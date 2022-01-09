d527c65802b05fcbd84da019e68a8fdb9d62e9f9
PatrickCraveiro <patrick.craveiro@medgrupo.com.br>
2022-01-08 21:58:50 -0300

    rodando npm run bundle junto com npm start

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,9 @@ const gulp = require("gulp");
 const clean = require("gulp-clean");
 const concat = require("gulp-concat");
 const sass = require("gulp-sass")(require("sass"));
-// const scan = require("gulp-scan");
-// const terser = require("gulp-terser");
-
-// CSS build
 
 function buildCss() {
-  let arrayFiles = [
-    `src/style/**/*.scss`,
-    //   `css/${type}/_scss/funcoes/*.scss`,
-    //   `css/${type}/_scss/**/*.scss`,
-  ];
+  let arrayFiles = [`src/style/**/*.scss`];
 
   let stream = gulp
     .src(arrayFiles)
@@ -21,51 +13,20 @@ function buildCss() {
 
   return stream.pipe(gulp.dest("dist"));
 }
-// function buildStaticCss() {
-//   return buildCss("static");
-// }
-// function buildDynamicCss() {
-//   return buildCss("dynamic");
-// }
 
-// function cleanDist() {
-//   return gulp
-//     .src(["build", "dist"], {
-//       allowEmpty: true,
-//     })
-//     .pipe(
-//       clean({
-//         read: false,
-//       })
-//     );
-// }
+function cleanDist() {
+  return gulp
+    .src(["dist"], {
+      allowEmpty: true,
+    })
+    .pipe(
+      clean({
+        read: false,
+      })
+    );
+}
 
 gulp.task(
   "bundle",
-  gulp.series(buildCss)
-  //   gulp.series(
-  //     gulp.parallel(
-  //       // Dynamic CSS (downloaded from server)
-  //       buildDynamicCss,
-  //       // Static CSS (bundled with app)
-  //       buildStaticCss,
-  //       // JS source
-  //       gulp.series(
-  //         gulp.parallel(
-  //           // Dynamic JS (downloaded from server)
-  //           'buildDynamicJs',
-  //           // Static JS (bundled with app)
-  //           buildStaticJs
-  //         ),
-  //         // Check module count
-  //         checkModuleCount
-  //       ),
-  //       // Electron Main.js
-  //       buildMainJs,
-  //       // Main page HTML (downloaded from server)
-  //       buildDynamicHtmlMainPage,
-  //       ...pagesComponents
-  //     )
-  //   )
-  // )
+  gulp.series(cleanDist, gulp.series(gulp.parallel(buildCss)))
 );
